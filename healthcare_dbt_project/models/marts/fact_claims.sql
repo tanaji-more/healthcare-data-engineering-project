1b900b6557aec85e6a19f5d699bce2c72d42190d
Tanaji More <tanajimore33@gmail.com>
2026-01-14 15:38:58 +0530

    added stg_claims as fact_claims table

---
+++ healthcare_dbt_project/models/marts/fact_claims.sql
@@ -0,0 +1,14 @@
+-- added stg_claims as fact_claims table
+
+{{ config (materialized= 'table') }}
+
+select 
+    claim_id,
+    claim_amount,
+    region,
+    smoker_flag,
+    bmi,
+    children,
+    age,
+    loaded_at
+from {{ ref ('stg_claims') }}
